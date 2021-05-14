4a189c356c9e0d7119ac038b4fa5b921b439d710
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-05-13 19:51:11 -0500

    Update index.js
    added more code--just learning :)

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const express = require("express");
 const app = express ();
 
 //call methods on "app" include .listen which starts the server 
-app.use(() => {
+//req = request res = response --these are both obj made by express 
+app.use((req, res) => {
     console.log("NEW REQUEST!!!")
+    //res.send("Yay! We got your request!! This is a response!")
+    //res.send({color: "red"}) //this is an object 
+    res.send("<h1>Hello World!</h1>") //this creates html in the browser
 })
 
 app.listen(3000, () => {
