2d26b323257052ccc38a0896801959074e4f58e1
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-05-13 19:21:13 -0500

    added comments for future reference

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+//recipe: 
+//1. npm install express
+//2. require express in file 
 const express = require("express"); 
 
+//3. execute express by saving to a variable (usually called "app")
 const app = express ();
 
+//call methods on "app" include .listen which starts the server 
 app.use(() => {
     console.log("NEW REQUEST!!!")
 })
